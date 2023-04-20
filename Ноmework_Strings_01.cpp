110d810a07f2ec3f5f9eff4547af348a671b08ce
Иван Степыгин <ivan.stepygin@playrix.com>
2023-04-20 16:12:03 +0300

    Add project files.

---
+++ Ноmework_Strings_01.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string name, lastname, result;
+	cout << "\nEnter your name: ";
+	cin >> name;
+	cout << "\nEnter your lastname: ";
+	cin >> lastname;
+	result = name + " " + lastname;
+	cout << "\nHello, " << result << "! Nice to meet you!\n";
+	return 0;
+}
