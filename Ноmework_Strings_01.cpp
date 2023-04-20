6935a08617361853fbb1f5db10a9927b93659416
Иван Степыгин <ivan.stepygin@playrix.com>
2023-04-20 16:19:55 +0300

    New cout What is your name

--- Ноmework_Strings_01.cpp
+++ Ноmework_Strings_01.cpp
@@ -4,9 +4,10 @@ using namespace std;
 int main()
 {
 	string name, lastname, result;
+	cout << "\t***What is your name?***\n";
 	cout << "\nEnter your name: ";
 	cin >> name;
-	cout << "\nEnter your lastname: ";
+	cout << "\nEnter your Lastname: ";
 	cin >> lastname;
 	result = name + " " + lastname;
 	cout << "\nHello, " << result << "! Nice to meet you!\n";
